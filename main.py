6b054afaa5374afa1c68f237a94720bc4fccfcb6
LostLukas <lukasdorji@gmail.com>
2025-07-15 12:54:28 +0200

    add(main) add textmail2service and service2checkmk to main

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import prepare
 import mail2text
+import textmail2service
+import service2checkmk
+
+
 
 def main() -> None:
     prepare.main()
     mails_processed: int = mail2text.main()
+    textmail2service.main()
+    service2checkmk.main()
+
 
 
 if __name__ == "__main__":
