c6db845b6db53655222a014e0450c21cdd46eb04
LostLukas <lukasdorji@gmail.com>
2025-07-21 15:00:33 +0200

    sorry, add: make stuff work

--- service2checkmk.py
+++ service2checkmk.py
@@ -1,39 +1,99 @@
+import os
 from pathlib import Path
-from textmail2service import Service, ServiceFile
+from re import sub
 
 
-
-def get_list_of_service_files() -> list[Path]:
-    """This return a list of every text file in service-files/ as Path objects"""
-    service_files: list[Path] = []
+def get_service_files() -> list[Path]:
+    """This returns a sorted list of every text file in service-files/ as Path objects"""
+    service_files_with_timestamp: list[tuple[Path, float]] = []
 
     for file in Path("service-files").glob("*.txt"):
-        service_files.append(file)
+        # This gets the timestamp from the filename
+        timestamp: float = float(file.stem.split("_")[-1].replace(",", "."))
+        service_files_with_timestamp.append((file, timestamp))
+    
+    sorted_service_files = sorted(service_files_with_timestamp, key=lambda x: x[1])
 
-    return service_files
+    # This returns the first element of the tuple t[0] for every tuple in sorted_service_files_with_timestamps
+    return [t[0] for t in sorted_service_files]
 
 
-def get_object_from_path(service_file: Path) -> ServiceFile:
-    ...
+# def get_object_from_path(service_files: list[Path]) -> list[Service]:
+#     ...
+
+def mark_as_processed(service_file_path: Path):
+    """This marks a service file as processed/for deletion"""
+    
+    lines: list[str] = []
+    with open(service_file_path, "r") as f:
+        lines = f.readlines()
+
+    lines[0] = "True\n"
+    with open(service_file_path, "w") as f:
+        f.writelines(lines)
+
 
 
 def filter_duplicate_services(service_files: list[Path]) -> list[Path]:
+    """This makes sure that only 1 service with the same name is returned in the list
+    and marks any duplicate as processed/for deletion"""
+
     list_of_service_names: list[str] = []
     filtered_service_files: list[Path] = []
 
     for service in service_files:
-        service_object: ServiceFile = get_object_from_path(service)
-        service_name: str = service_object.name
+        # service_object: Service = get_object_from_path(service)
+        # service_name: str = service_object.name
+        filename_stem: str = service.stem
+        service_name: str = sub(r"_\d{10,},", "", filename_stem)
+
         if service_name not in list_of_service_names:
             list_of_service_names.append(service_name)
             filtered_service_files.append(service)
+        else:
+            mark_as_processed(service)
+            
 
     return filtered_service_files
 
 
+def send_to_checkmk(service_files: list[Path]) -> None:
+    """This prints the second line to standard out"""
+
+    for file in service_files:
+        checkmk_output: list[str] = file.read_text().splitlines()
+        print(checkmk_output[1])
+
+def mark_services_with_ok_status_as_processed(filtered_service_files: list[Path]) -> None:
+    """This sends any service file that has a 0 status code to mark_as_processed()"""
+    
+    for file in filtered_service_files:
+        lines: list[str] = []
+        with open(file, "r") as f:
+            lines = f.readlines()
+        # The status code of a service file is saved in the first position of the second line
+        if lines[1][0] == "0":
+            mark_as_processed(file)
+
+
+def delete_service_files(service_files: list[Path]) -> None:
+    """This deletes every file that has been marked as processed/for deletion"""
+
+    for file in service_files:
+        lines: list[str] = []
+        with open(file, "r") as f:
+            lines = f.readlines()
+        if lines[0] == "True\n":
+            os.remove(file.absolute().as_posix())
+
+
 
 def main() -> None:
-    service_files: list[Path] = get_list_of_service_files()
+    service_files: list[Path] = get_service_files()
+    filtered_service_files: list[Path] = filter_duplicate_services(service_files)
+    send_to_checkmk(filtered_service_files)
+    mark_services_with_ok_status_as_processed(filtered_service_files)
+    delete_service_files(service_files)
 
 
 
--- textmail2service.py
+++ textmail2service.py
@@ -1,8 +1,12 @@
+import os
 from pathlib import Path
 from configparser import ConfigParser, SectionProxy
-from mail2text import Email
 from dataclasses import dataclass
-import re
+from re import search, sub, Match
+from time import time
+
+from mail2text import Email
+
 
 
 def get_plaintext_emails() -> list[Path]:
@@ -11,7 +15,7 @@ def get_plaintext_emails() -> list[Path]:
     plaintext_email_directory = Path("plaintext-emails")
     plaintext_email_list = list(plaintext_email_directory.glob("*.txt"))
     # This sorts the list with the unix timestamp from the filename
-    return sorted(plaintext_email_list, key=lambda p: int(p.stem.split("_")[-1]))
+    return sorted(plaintext_email_list, key=lambda p: float(p.stem.split("_")[-1].replace(",", ".")))
 
 
 def get_service_configs() -> list[SectionProxy]:
@@ -51,38 +55,190 @@ def get_email_from_path(email_path: Path) -> Email:
 
 @dataclass
 class Service:
+    processed: bool
     status: int
     name: str
     values: dict
     status_details: str
 
-@dataclass
-class ServiceFile(Service):
-    processed: bool
+
+
+def create_service_object(service_config: SectionProxy, email_object: Email, subject_match: Match, timestamp: float) -> Service | None:
+    """This returns a Service object from the passed info that is fully parsed"""
+
+    status: int = 3
+
+    ok_regex = service_config.get("ok_regex")
+    warn_regex = service_config.get("warn_regex")
+    crit_regex = service_config.get("crit_regex")
+
+    # ok_match: Match | None = None
+    # warn_match: Match | None = None
+    # crit_match: Match | None = None
+
+    # if ok_regex is not None:
+    #     ok_match = search(ok_regex, email_object.body)
+    # if warn_regex is not None:
+    #     warn_match = search(warn_regex, email_object.body)
+    # if crit_regex is not None:
+    #     crit_match = search(crit_regex, email_object.body)
+
+    ok_match = search(ok_regex, email_object.body) if ok_regex else None
+    warn_match = search(warn_regex, email_object.body) if warn_regex else None
+    crit_match = search(crit_regex, email_object.body) if crit_regex else None
+    relevant_match: Match = search("", email_object.body)
+
+    if ok_match is not None:
+        status = 0
+        relevant_match = ok_match
+    elif warn_match is not None:
+        status = 1
+        relevant_match = warn_match
+    elif crit_match is not None:
+        status = 2
+        relevant_match = crit_match
+    else:
+        return None
+
+    crit_cycle: int = service_config.getint("crit_cycle")
+    time_difference: float = time() - timestamp
+    if crit_cycle < time_difference:
+        status = 2
+
+    name: str = service_config.get("name").replace("EMAIL_SUBJECT_REGEX", subject_match.group(1))
+
+    values: dict = {}
+
+    # print(warn_match)
+
+    details: str = ""
+    match status:
+        case 0:
+            details = (service_config.get("ok_details")
+                       .replace("EMAIL_SUBJECT_REGEX", subject_match.group(1))
+                       .replace("REGEX_GROUP", relevant_match.group(1))
+                       )
+        case 1:
+            details = (service_config.get("warn_details")
+                       .replace("EMAIL_SUBJECT_REGEX", subject_match.group(1))
+                       .replace("REGEX_GROUP", relevant_match.group(1))
+                       )
+        case 2:
+            details = (service_config.get("crit_details")
+                       .replace("EMAIL_SUBJECT_REGEX", subject_match.group(1))
+                       .replace("REGEX_GROUP", relevant_match.group(1))
+                       )
+
+    processed: bool = False
+            
+    return Service(processed, status, name, values, details)
 
 
 
-def process_emails(plaintext_emails_paths: list[Path], service_config_list: list[SectionProxy]) -> None:
+def process_emails(plaintext_emails_paths: list[Path], service_config_list: list[SectionProxy]) -> tuple[list[Service], list[Path], int, int]:
+    """This checks if any service config applies to the email subject, converts them to Service objects with create_service_object()
+    and returns a tuple with the Service object list and a bunch of other useful information"""
+
+    service_objects: list[Service] = []
+    service_files_created: int = 0
+    files_to_be_deleted: list[Path] = []
+    email_without_service_count: int = 0
 
     for email_path in plaintext_emails_paths:
         email_object = get_email_from_path(email_path)
+        email_processed = False
 
         # print(email_object)
         for service_config in service_config_list:
             email_subject_regex = service_config.get("email_subject_regex")
             # print(email_subject_regex)
             # print(email_object.subject)
-            re_match = re.match(email_subject_regex, email_object.subject)
+            re_match: Match = search(email_subject_regex, email_object.subject)
             # print(re_match)
-            if re_match != None:
-                ...
-                # print("CHECK")
+            if re_match is not None:
+                # We saved the timestamp in the filename with a "," as the seperator
+                timestamp: float = float(email_path.stem.split("_")[-1].replace(",", "."))
+                service_object: Service | None = create_service_object(service_config, email_object, re_match, timestamp)
+                if service_object is not None:
+                    service_objects.append(service_object)
+                    service_files_created += 1
+                    # print(service_object)
+                    email_processed = True
+        if email_processed:
+            files_to_be_deleted.append(email_path)
+        else:
+            email_without_service_count += 1
+
+    return service_objects, files_to_be_deleted, service_files_created, email_without_service_count
 
 
-def main() -> None:
+
+def checkmk_services(service_files: list[Service], emails_processed: int, service_files_created: int, email_without_service_count: int) -> list[Service]:
+    """This adds checkmk related services to the service list"""
+
+    service_files.append(Service(True,
+                                 0,
+                                 "[Mail2CheckMK] Stats",
+                                 {"emails_processed":emails_processed, "service_files_created":service_files_created},
+                                 f"Mail2CheckMK processed {emails_processed} mails and created {service_files_created} service files this run."
+                                 )
+                         )
+    
+    if email_without_service_count > 0:
+        service_files.append(Service(True,
+                                     1,
+                                     "[Mail2CheckMK] Emails without service",
+                                     {"email_without_service_count":email_without_service_count},
+                                     f"Mail2CheckMK couldn't find services for {email_without_service_count} email(s)"
+                                     )
+                             )
+
+    return service_files
+
+
+def save_service_files(service_objects: list[Service]) -> None:
+
+    for service_object in service_objects:
+        filename: str = sub(r"[\/\0\n\r\s]", "_", service_object.name)
+        unix_time_string = str(time()).replace(".", ",")
+        filename += f"_{unix_time_string}"
+        formatted_dict: str = ""
+        for key, value in service_object.values.items():
+            if formatted_dict == "":
+                formatted_dict += f"{key}={value}"
+            else:
+                formatted_dict += f"|{key}={value}"
+            
+
+        with open(f"service-files/{filename}.txt", "w") as file:
+            file.write(f"{service_object.processed}\n")
+            file.write(f'{str(service_object.status)} "{service_object.name}" {formatted_dict} {service_object.status_details}')
+
+
+def delete_files(files_to_be_deleted: list[Path]) -> None:
+    """This deletes every file in the list"""
+
+    for file in files_to_be_deleted:
+        if file.exists():
+            os.remove(file.absolute().as_posix())
+
+def move_mails_without_service(plaintext_email_paths: list[Path]) -> None:
+    """This moves all remaining plaintext-emails into the without-service subdirectory,
+    but because the only remaining one's don't have services it's called like this."""
+
+    for file in plaintext_email_paths:
+        if file.exists():
+            file.rename(file.parent / "unprocessed-emails" / file.name)
+
+
+def main(emails_saved: int = 0) -> None:
     plaintext_emails_paths: list[Path] = get_plaintext_emails()
     service_config_list: list[SectionProxy] = get_service_configs()
-    process_emails(plaintext_emails_paths, service_config_list)
+    service_objects, files_to_be_deleted, service_files_created, email_without_service_count = process_emails(plaintext_emails_paths, service_config_list)
+    service_objects: list[Service] = checkmk_services(service_objects, emails_saved, service_files_created, email_without_service_count)
+    save_service_files(service_objects)
+    delete_files(files_to_be_deleted)
+    move_mails_without_service(plaintext_emails_paths)
 
 
 if __name__ == "__main__":
