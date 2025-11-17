1f476f34feb6fa5091f57c650abe5efaa10f2bf1
LostLukas <lukasdorji@gmail.com>
2025-11-17 17:58:21 +0100

    change(launch): look for project in /opt

--- mail2checkmk.sh
+++ mail2checkmk.sh
@@ -2,16 +2,16 @@
 set -e
 
 # check if the virtualenv exists
-if [ ! -d ".venv/bin/" ]; then
+if [ ! -d "/opt/Mail2CheckMk/.venv/bin/" ]; then
     echo "Virtual environment not found, aborting."
     exit 1
 fi
 
 # check if the main python file exists
-if [ ! -f "./main.py" ]; then
+if [ ! -f "/opt/Mail2CheckMk/main.py" ]; then
     echo "Python script not found, aborting."
     exit 1
 fi
 
 # Run mail2checkmk
-exec .venv/bin/python main.py
+exec /opt/Mail2CheckMk/.venv/bin/python /opt/Mail2CheckMk/main.py
