4147b6fed15e526102d24b1fa8f6e837648482d9
LostLukas <lukasdorji@gmail.com>
2025-12-14 19:04:50 +0100

    fix(prepare): set correct working directory

--- prepare.py
+++ prepare.py
@@ -2,9 +2,11 @@
 # for the other modules exists
 
 from pathlib import Path
+from os import chdir
 
 def main() -> None:
 
+    chdir("/opt/Mail2CheckMk")
     Path("plaintext-emails/without-service").mkdir(parents=True, exist_ok=True)
     Path("service-files").mkdir(parents=True, exist_ok=True)
 
